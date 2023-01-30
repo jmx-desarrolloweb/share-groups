31175014c207fd1e6cb3b8be62bddb34fb856aeb
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-01-30 14:24:10 -0600

    Botones principales del NavBar reordenados

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -124,34 +124,6 @@ export const NavBar:FC<Props> = ({ category }) => {
                 </div>
                 <div className='flex items-stretch gap-2'>
                     <button
-                        onClick={ ()=> handleResetRandom( false ) }
-                        disabled={ loadingResetAll || loadingReset } 
-                        className='group text-sm rounded-md px-2 sm:py-2 sm:px-6 border-2 sm:border-[3px] border-indigo-600 hover:shadow-lg transition-transform flex items-center justify-center gap-1 disabled:opacity-50 disabled:shadow-none'
-                    >
-                        {
-                            loadingResetAll
-                                ?(
-                                <svg className="animate-spin h-7 sm:h-8 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <circle
-                                        className="opacity-25"
-                                        cx="12"
-                                        cy="12"
-                                        r="10"
-                                        stroke="currentColor"
-                                        strokeWidth="4">
-                                    </circle>
-                                    <path
-                                        className="opacity-75"
-                                        fill="currentColor"
-                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                                    </path>
-                                </svg>
-                                ):(
-                                    <i className='bx bx-collection text-xl sm:text-2xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 to-blue-700 group-hover:font-semibold group-disabled:group-hover:font-normal' ></i>
-                                )
-                        }
-                    </button>
-                    <button
                         disabled={ loadingResetAll || loadingReset }
                         onClick={ ()=> handleResetRandom( true ) } 
                         className='font-semibold text-sm rounded-md px-3 sm:py-2 sm:px-6 bg-gradient-to-r from-indigo-700 to-blue-700 enabled:hover:from-indigo-800 enabled:hover:to-blue-800 text-white hover:shadow-lg transition-transform flex items-center justify-center gap-1 disabled:opacity-50 disabled:shadow-none'
@@ -176,6 +148,34 @@ export const NavBar:FC<Props> = ({ category }) => {
                                 </svg>
                                 ):(
                                     <i className='bx bx-refresh text-xl sm:text-2xl' ></i>
+                                )
+                        }
+                    </button>
+                    <button
+                        onClick={ ()=> handleResetRandom( false ) }
+                        disabled={ loadingResetAll || loadingReset } 
+                        className='group text-sm rounded-md px-2 sm:py-2 sm:px-6 border-2 sm:border-[3px] border-indigo-600 hover:shadow-lg transition-transform flex items-center justify-center gap-1 disabled:opacity-50 disabled:shadow-none'
+                    >
+                        {
+                            loadingResetAll
+                                ?(
+                                <svg className="animate-spin h-7 sm:h-8 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle
+                                        className="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        strokeWidth="4">
+                                    </circle>
+                                    <path
+                                        className="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                                    </path>
+                                </svg>
+                                ):(
+                                    <i className='bx bx-collection text-xl sm:text-2xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 to-blue-700 group-hover:font-semibold group-disabled:group-hover:font-normal' ></i>
                                 )
                         }
                     </button>
